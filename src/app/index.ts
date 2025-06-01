4b4869fb0caa5c495c675b44293f319521b35894
MinnieMinwoo(Minwoo Pak) <nhs075241@snu.ac.kr>
2025-06-01 15:32:54 +0900

    fi: fix index.ts

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,12 +1,10 @@
-// filepath: /Users/minnie/Develop/Visual Studio Code/DoorUnlocker/src/server.ts
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import { createRoot } from "react-dom/client";
 
-dotenv.config(); // .env 파일이 있다면 환경 변수를 로드합니다.
+dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 3000; // 환경 변수 PORT를 사용하거나 기본값 3001을 사용
+const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send();
